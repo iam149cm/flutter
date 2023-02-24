8eea4704eccac311134bc2ecbb3e827c6b5af97f
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-02-24 19:23:22 +0900

    add jsonDecoder, .env setting

--- 18_clima/lib/screens/loading_screen.dart
+++ 18_clima/lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -26,9 +30,35 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    var uri = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=44aa6ccca698c1bfd7fdc89e9ecb4e2f");
+
+    http.Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      var weatherDesciption = decodedData['weather'][0]['description'];
+
+      var temperature = decodedData['main']['temp'];
+      var condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(weatherDesciption);
+
+      await DotEnv.load(fileName: "key.env");
+      print(DotEnv.env['SUPER_SECRET_API_KEY']);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     print('✨ build...................');
+    getData();
     return Scaffold();
   }
 }
