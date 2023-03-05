97afb9ed8a1710d50d92866da9b4952c7d5f69e5
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-03-05 23:19:40 +0900

    add the ListView Builder

--- 20_todoey/lib/widgets/tasks_list.dart
+++ 20_todoey/lib/widgets/tasks_list.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/models/task.dart';
 import 'package:todoey/widgets/tasks_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     super.key,
   });
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      TaskTile(),
-      TaskTile(),
-      TaskTile(),
-    ]);
+    return ListView.builder(
+      itemCount: tasks.length, // index 에러 방지를 위해 필요한 property
+      itemBuilder: (context, index) {
+        return TaskTile(
+            isChecked: tasks[index].isDone,
+            taskTitle: tasks[index].name,
+            checkboxCallback: (bool? checkboxState) {
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            });
+      },
+    );
   }
 }
--- 20_todoey/lib/widgets/tasks_tile.dart
+++ 20_todoey/lib/widgets/tasks_tile.dart
@@ -7,27 +7,32 @@ import 'package:flutter/material.dart';
 🤍 StatefulWidget - 상황에 따라 폈다 접었다 할 수 있는 변신 테이블. final 로 변수 선언 불가 (state 가 바뀌어야 하니까)
 🤍 StatelessWidget - 그냥 테이블
 */ ///
-
-class TaskTile extends StatefulWidget {
-  const TaskTile({
-    super.key,
-  });
-
+///
+// 💛 task를 분리함으로써 stateless widget 으로 변경
+class TaskTile extends StatelessWidget {
   // 🤍 1. state 로 부터 statefulWidget 을 create
-  @override
-  State<TaskTile> createState() => _TaskTileState();
-}
+  // @override
+  // State<TaskTile> createState() => _TaskTileState();
+// }
 
 // 🤍 2. 실제 state object. 값이 바뀌는 변수가 들어있다.
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false; // 👈 실제 state를 변경할 변수는 statefulWidget 안에 들어있다
+// class _TaskTileState extends State<TaskTile> {
+  final bool isChecked; // 👈 실제 state를 변경할 변수는 statefulWidget 안에 들어있다
+  final String taskTitle;
+  final Function(bool?) checkboxCallback;
+
+  TaskTile({
+    required this.isChecked,
+    required this.taskTitle,
+    required this.checkboxCallback,
+  });
 
   // 하위 위젯에서 체크박스 클릭 시 호출 할 콜백함수. 일반적으로는 익명함수로 사용하는 경우가 많다
-  void checkBoxCallback(bool? checkboxState) {
-    setState(() {
-      isChecked = checkboxState ?? false;
-    });
-  }
+  // void checkBoxCallback(bool? checkboxState) {
+  //   setState(() {
+  //     isChecked = checkboxState ?? false;
+  //   });
+  // }
 
   // 🤍 3. 변수의 state가 바뀌면 build 메소드에서 UI 를 업데이트 해 준다.
   @override
@@ -35,38 +40,40 @@ class _TaskTileState extends State<TaskTile> {
     return ListTile(
       // title - 처음에 무엇이 올 것인가
       title: Text(
-        'This is a task. 1',
+        taskTitle,
         style: TextStyle(
             decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
       // trailing - 마지막에 무엇이 올 것인가
-      trailing: TaskCheckbox(
-        checkboxState: isChecked,
-        toggleCheckboxState: checkBoxCallback,
+      trailing: Checkbox(
+        shape: CircleBorder(),
+        value: isChecked,
+        activeColor: Colors.teal.shade200,
+        onChanged: checkboxCallback, // checkbox 가 변경될 때 callback 이 trigger 된다
       ),
     );
   }
 }
 
 // 🤍 4. final 이므로 여기서 요소의 state를 변경할 수는 없다.
-class TaskCheckbox extends StatelessWidget {
-  final bool checkboxState;
-  final Function(bool?) toggleCheckboxState; // 호출할 콜백함수의 자료형과 맞춰주었다
+// class TaskCheckbox extends StatelessWidget {
+//   final bool checkboxState;
+//   final Function(bool?) toggleCheckboxState; // 호출할 콜백함수의 자료형과 맞춰주었다
 
-  // 🤍 5. 그러나 업데이트가 필요하면 생성자를 통해 새롭게 초기화하고 이전의 것은 버린다
-  // 🤍 6. Lifted up to the parent widget (TaskCheckbox --> TaskTile) 즉, 위젯트리의 부모 노드로 값을 올린다.
-  const TaskCheckbox(
-      {super.key,
-      required this.checkboxState,
-      required this.toggleCheckboxState});
+// 🤍 5. 그러나 업데이트가 필요하면 생성자를 통해 새롭게 초기화하고 이전의 것은 버린다
+// 🤍 6. Lifted up to the parent widget (TaskCheckbox --> TaskTile) 즉, 위젯트리의 부모 노드로 값을 올린다.
+// const TaskCheckbox(
+//     {super.key,
+//     required this.checkboxState,
+//     required this.toggleCheckboxState});
 
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      shape: CircleBorder(),
-      value: checkboxState,
-      activeColor: Colors.teal.shade200,
-      onChanged: toggleCheckboxState, // checkbox 가 변경될 때 callback 이 trigger 된다
-    );
-  }
-}
+// @override
+// Widget build(BuildContext context) {
+//   return Checkbox(
+//     shape: CircleBorder(),
+//     value: checkboxState,
+//     activeColor: Colors.teal.shade200,
+//     onChanged: toggleCheckboxState, // checkbox 가 변경될 때 callback 이 trigger 된다
+//   );
+// }
+// }
